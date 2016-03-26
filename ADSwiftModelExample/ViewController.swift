fd89381c8bf43bad42a5165cf919a7583aa8d72d
安德航 <bjwltiankong@163.com>
2016-03-26 16:33:30 +0800

    Initial Commit

---
+++ ADSwiftModelExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ADSwiftModelExample
+//
+//  Created by bjwltiankong on 16/3/26.
+//  Copyright © 2016年 bjwltiankong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
